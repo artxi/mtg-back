51b0ffa0ba74d3d6613fb53314498c75f6440774
artzai <aartxi@gmail.com>
2023-07-21 06:43:50 +0200

    Add basic card search

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('card/:name')
+  getCard(@Param() { name }): Promise<any> {
+    return this.appService.getCard(name);
+  }
+
+  @Get('search/:name')
+  searchCard(@Param() { name }): Promise<any> {
+    return this.appService.searchCard(name);
   }
 }
