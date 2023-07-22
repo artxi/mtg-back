797825d83023944a05b98d3fc7de0e37c55db356
artzai <aartxi@gmail.com>
2023-07-22 07:14:12 +0200

    Unhandled error handling

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+process.on('uncaughtException', (error: any) => {
+  console.log(error.stack);
+});
+
+process.on('unhandledRejection', (error: any) => {
+  console.log(error.stack);
+});
+
+process.on('UnhandledPromiseRejectionWarning', (error: any) => {
+  console.log(error.stack);
+});
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
