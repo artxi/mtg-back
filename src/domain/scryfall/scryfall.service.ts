0812b60b6658afa216ad78b226155b8fd7e4245c
artzai <artzai@crowdfarming.com>
2023-08-01 08:57:40 +0200

    Restart set & scryfall services

--- src/domain/scryfall/scryfall.service.ts
+++ src/domain/scryfall/scryfall.service.ts
@@ -1,40 +1,44 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom, catchError, map } from 'rxjs';
 
-const scryfallUri = 'https://api.scryfall.com';
+const baseScryfallUri = 'https://api.scryfall.com';
 
 @Injectable()
 export class ScryfallService {
   constructor(private httpService: HttpService) {}
 
-  async getCardByName(name: string) {
-    return this.callScryfallApi(`${scryfallUri}/cards/named?exact=${name}`);
+  async sets(
+    args?: {
+      code?: string;
+      scryfallId?: string;
+      tcgPlayerId?: number;
+    },
+    params?: {
+      format?: string;
+      pretty?: boolean;
+    }
+  ) {
+    let uri = '/sets';
+
+    if (args?.code) {
+      uri += '/' + args.code;
+    }
+
+    if (args?.scryfallId) {
+      uri += '/' + args.scryfallId;
+    }
+
+    if (args?.tcgPlayerId) {
+      uri += '/tcgplayer/' + args.tcgPlayerId;
+    }
+
+    return this.callScryfallApi(uri, params);
   }
 
-  async searchCardsByName(name: string) {
-    // This will probably need pagination at some point. Response contains has_more attribute
-    const response = await this.callScryfallApi(
-      `${scryfallUri}/cards/search?q=%21"${name}"+include%3Aextras&unique=prints`
-    );
-
-    return response.data;
-  }
-
-  async getSetByCode(code: string) {
-    return this.callScryfallApi(`${scryfallUri}/sets/${code}`);
-  }
-
-  async listSets() {
-    // This will probably need pagination at some point. Response contains has_more attribute
-    const response = await this.callScryfallApi(`${scryfallUri}/sets`);
-
-    return response.data;
-  }
-
-  private async callScryfallApi(uri: string) {
+  private async callScryfallApi(uri: string, params = {}) {
     const request = this.httpService
-      .get(uri)
+      .get(baseScryfallUri + uri, params)
       .pipe(map((response) => response.data))
       .pipe(
         catchError((e) => {
--- src/domain/set/set.service.ts
+++ src/domain/set/set.service.ts
@@ -1,67 +1,30 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Set } from './set.schema';
-import { Model } from 'mongoose';
+// import { InjectModel } from '@nestjs/mongoose';
+// import { Set } from './set.schema';
+// import { Model } from 'mongoose';
 import { ScryfallService } from '../scryfall/scryfall.service';
-import { CardService } from '../card/card.service';
-import { Card } from '../card/card.schema';
-import { CreateSetDto } from './dto/set.create.dto';
+// import { CreateSetDto } from './dto/set.create.dto';
 
 @Injectable()
 export class SetService {
   constructor(
-    @InjectModel(Set.name) private setModel: Model<Set>,
-    private scryfallService: ScryfallService,
-    private cardService: CardService
+    // @InjectModel(Set.name) private setModel: Model<Set>,
+    private scryfallService: ScryfallService
   ) {}
 
-  async findByCode(code: string): Promise<Set> {
-    const expression = new RegExp(code, 'i');
-    return this.setModel.findOne({ code: expression }).exec();
+  async byCode(code: string) {
+    return this.scryfallService.sets({ code: code });
   }
 
-  async create(createSetDto: CreateSetDto): Promise<Set> {
-    const createdSet = new this.setModel(createSetDto);
-    return createdSet.save();
+  async byScryfallId(id: string) {
+    return this.scryfallService.sets({ scryfallId: id });
   }
 
-  async listSets(): Promise<Set[]> {
-    return this.list(
-      {},
-      { name: 1, code: 1, date: '$released_at', image: '$icon_svg_uri' }
-    );
+  async byTcgPlayerId(id: number) {
+    return this.scryfallService.sets({ tcgPlayerId: id });
   }
 
-  async list(query, projection): Promise<Set[]> {
-    return this.setModel.find(query, projection).exec();
-  }
-
-  async getSetByCode(code: string): Promise<Set> {
-    let set = await this.findByCode(code);
-
-    if (set) {
-      return set;
-    }
-
-    const scryfallSet = await this.scryfallService.getSetByCode(code);
-
-    scryfallSet.scryfallId = scryfallSet.id;
-    delete scryfallSet.id;
-
-    set = await this.create(scryfallSet);
-
-    return set;
-  }
-
-  async getSetCards(code: string, params): Promise<Card[]> {
-    const lang = params.lang || 'en';
-
-    return this.cardService.list(
-      { set: code, lang },
-      {
-        name: 1,
-        image: '$image_uris.normal'
-      }
-    );
+  async list() {
+    return this.scryfallService.sets();
   }
 }
